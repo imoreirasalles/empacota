a2b6ba569b6a601c03ffbc0cf23b0344d668b96d
buccalon <buccalon@users.noreply.github.com>
2018-04-06 16:07:46 -0300

    Escrita de várias colunas no excel
    O código anterior não conseguia lidar com .xml longos. Agora não há limite na criação de colunas no .xlsx

--- empacota.py
+++ empacota.py
@@ -1,9 +1,10 @@
 from xml.etree import ElementTree as ET
 from openpyxl import Workbook
+from openpyxl.utils import get_column_letter, column_index_from_string
 
 with open("arquivo.xml", "rt") as f:
    tree = ET.parse(f)
-   filename = f.name 
+   filename = f.name
 root = tree.getroot()
 
 ns = { 'cumulus' : 'http://www.canto.com/ns/Export/1.0'}
@@ -22,8 +23,8 @@ def mk_header():
    for node in root[0][0].findall('cumulus:Field', ns):
       #print(node.attrib, node.text)
       campo = node.find('cumulus:Name', ns)
-      ws[ col + str(row)] = campo.text
-      col = chr(ord(col) + 1)
+      ws[ col + str(row) ] = campo.text
+      col = get_column_letter(column_index_from_string(col) + 1)
       uidcampo[node.attrib['uid']] = campo.text
    return uidcampo
 
@@ -32,13 +33,13 @@ def fill_record(dic):
    count = 0
    for ITEM in root[1]:
       count += 1
-      col = 'A'
       for FieldValue in ITEM.findall('cumulus:FieldValue', ns):
          if FieldValue.attrib['uid'] in dic.keys():
             chave = FieldValue.attrib['uid']
             for campo in FieldValue.iter():
-                col = chr( 97 + list(dic.keys()).index(chave) )
-                ws[ col + str(row)] = campo.text
+                if list(dic.keys()).index(chave) != 0:
+                    col = get_column_letter(list(dic.keys()).index(chave) + 1)
+                    ws[ col + str(row)] = campo.text
       row += 1
    return count
 
